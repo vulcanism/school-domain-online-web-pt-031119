4b1228ec1c4c9ef4791f7ca6491752d56f44acc3
Savannah C <sgcorrell@gmail.com>
2019-03-29 18:12:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,7 +9,7 @@ class School
   def add_student(name, grade)
     
     if @roster[grade] != nil
-      @roster[grade] << name
+       @roster[grade] << name
     else
       @roster[grade] = [name]
   end
